7428a7480ad1de0d616386efa8b1af3ce2d901f1
Bhanu <Bhanu@DESKTOP-5CT2JOV>
2023-01-14 00:37:50 +0530

    First Commit

---
+++ DemoProgram/src/javaPrograms/AddNumber.java
@@ -0,0 +1,14 @@
+package javaPrograms;
+
+public class AddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20,sum=0;
+		sum=a+b;
+		System.out.println("Sum of two number:"+sum);
+
+	}
+
+}
---
+++ DemoProgram/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Bhanu
+ *
+ */
+module DemoProgram {
+}
