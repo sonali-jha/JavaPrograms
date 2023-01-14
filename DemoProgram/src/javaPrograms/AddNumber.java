b939c6cc18436f121c0da280cda853a943832c51
Bhanu <Bhanu@DESKTOP-5CT2JOV>
2023-01-14 15:17:10 +0530

    update

--- DemoProgram/src/javaPrograms/AddNumber.java
+++ DemoProgram/src/javaPrograms/AddNumber.java
@@ -4,7 +4,7 @@ public class AddNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=20;
+		int a=10;
 		int b=30,sum=0;
 		sum=a+b;
 		System.out.println("Sum of two number:"+sum);
